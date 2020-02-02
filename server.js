518407dcd16fe6adab7e61cd0acb20efc7aa18fb
NicoleVenachi <ludwing@unicauca.edu.co>
2020-02-01 20:28:49 -0500

    Router de express

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express')
+
 // import express from express
+var app = express () //instacio  modulo express
 
-var app = express ()
+const router = express.Router() // instancio router de express
+app.use(router) //defino que router hara veces de .use
 
-app.use('/', function(req, res) {
-  res.send('Hola')
+router.get('/message', function(req, res) {
+  res.send('Lista de mensajes')
 })
 
-app.listen(3000)
+router.post('/message', function(req, res) {
+  res.send('Mensaje añadido')
+})
+//
+// app.use('/', function(req, res) {
+//   res.send('Hola') //la response sera un hoa
+// }) //defino peticion http al acceder a la raiz
+//
+app.listen(3000) //defino el puerto por el qu escucho peticiones
 console.log('La aplicación está escuchando en htttp://localhots:3000')
